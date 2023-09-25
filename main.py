5dcbcf5facbbce5e7af2e94357b4725f7de0399a
Indra Wahyu <wicaksana0701@gmail.com>
2023-09-26 02:46:10 +0800

    nice basic akar kuadrat

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Inisialisasi templates
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def show_form(request: Request):
+    return templates.TemplateResponse("input_form.html", {"request": request})
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/hitung-akar-kuadrat")
+async def hitung_akar_kuadrat(request: Request, angka: float = Form(...)):
+    if angka < 0:
+        return {"error": "Angka harus positif"}
+
+    akar = angka**0.5
+    return templates.TemplateResponse(
+        "output_result.html", {"request": request, "hasil": akar}
+    )
